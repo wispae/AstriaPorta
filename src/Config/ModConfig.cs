ddc04b07baa704d124f279e484e19137ee5b530b
wispae <wilferspaepen@gmail.com>
2025-04-05 20:21:52 +0200

    Add initial mod config

---
+++ src/Config/ModConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstriaPorta.Config
+{
+	public class StargateConfig
+	{
+		public static StargateConfig Loaded { get; set; } = new StargateConfig();
+
+		public bool DhdDestructable { get; set; } = true;
+		public int DhdMiningTier { get; set; } = 4;
+		public bool StargateDestructable { get; set; } = true;
+		public int StargateMiningTier { get; set; } = 5;
+		public bool VortexDestroys { get; set; } = true;
+		public bool VortexKills { get; set; } = true;
+		public float MaxTimeoutSeconds { get; set; } = 10f;
+		public int MinRangeChunksMilkyway { get; set; } = 0;
+		public int MaxRangeChunksMilkyway { get; set; } = 262144;
+		public float MaxConnectionDurationSecondsMilkyway { get; set; } = 60f;
+		public float DialSpeedDegreesPerSecondMilkyway { get; set; } = 80f;
+		public bool AllowQuickDial { get; set; } = true;
+	}
+}
