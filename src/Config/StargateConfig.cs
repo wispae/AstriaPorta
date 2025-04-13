ed2802281b81140339ff7545bc05e0d43f5ef7db
wispae <wilferspaepen@gmail.com>
2025-04-13 16:37:09 +0200

    Add stargate spawn distance to config

--- src/Config/StargateConfig.cs
+++ src/Config/StargateConfig.cs
@@ -9,81 +9,107 @@ using System.Threading.Tasks;
 
 namespace AstriaPorta.Config
 {
-	[ProtoContract]
-	public class StargateConfig
-	{
-		[JsonIgnore]
-		private int _minRangeChunksMilkyway = 10;
-		[JsonIgnore]
-		private int _maxRangeChunksMilkyway = 262144;
-		[JsonIgnore]
-		private float _maxConnectionDurationSecondsMilkyway = 60f;
-		[JsonIgnore]
-		private float _dialSpeedDegreesPerSecondMilkyway = 80f;
+    [ProtoContract]
+    public class StargateConfig
+    {
+        [JsonIgnore]
+        private int _minRangeChunksMilkyway = 10;
+        [JsonIgnore]
+        private int _maxRangeChunksMilkyway = 262144;
+        [JsonIgnore]
+        private float _maxConnectionDurationSecondsMilkyway = 60f;
+        [JsonIgnore]
+        private float _dialSpeedDegreesPerSecondMilkyway = 80f;
+        [JsonIgnore]
+        private int _minDistanceSurfaceGates = 1000;
+        [JsonIgnore]
+        private int _minDistanceUndergroundGates = 1000;
 
-		public static StargateConfig Loaded { get; set; } = new StargateConfig();
+        public static StargateConfig Loaded { get; set; } = new StargateConfig();
 
-		[ProtoMember(1), DefaultValue(true)]
-		public bool DhdDestructable { get; set; } = true;
-		[ProtoMember(2), DefaultValue(4)]
-		public int DhdMiningTier { get; set; } = 4;
-		[ProtoMember(3), DefaultValue(true)]
-		public bool StargateDestructable { get; set; } = true;
-		[ProtoMember(4), DefaultValue(5)]
-		public int StargateMiningTier { get; set; } = 5;
-		[ProtoMember(5), DefaultValue(true)]
-		public bool VortexDestroys { get; set; } = true;
-		[ProtoMember(6), DefaultValue(true)]
-		public bool VortexKills { get; set; } = true;
-		[ProtoMember(7), DefaultValue(5f)]
-		public float MaxTimeoutSeconds { get; set; } = 5f;
-		[ProtoMember(8), DefaultValue(10)]
-		public int MinRangeChunksMilkyway
-		{
-			get => _minRangeChunksMilkyway;
-			set
-			{
-				if (value > _maxRangeChunksMilkyway) value = _maxRangeChunksMilkyway - 1;
-				if (value < 0) value = 0;
-				if (value > 262144) value = 262144;
-				_minRangeChunksMilkyway = value;
-			}
-		}
-		[ProtoMember(9), DefaultValue(262144)]
-		public int MaxRangeChunksMilkyway
-		{
-			get => _maxRangeChunksMilkyway;
-			set
-			{
-				if (value < _minRangeChunksMilkyway) value = _minRangeChunksMilkyway + 1;
-				if (value < 0) value = 0;
-				if (value > 262144) value = 262144;
-				_maxRangeChunksMilkyway = value;
-			}
-		}
-		[ProtoMember(10), DefaultValue(60f)]
-		public float MaxConnectionDurationSecondsMilkyway
-		{
-			get => _maxConnectionDurationSecondsMilkyway;
-			set
-			{
-				if (value < 10f) value = 10f;
-				if (value > 180f) value = 180f;
-				_maxConnectionDurationSecondsMilkyway = value;
-			}
-		}
-		[ProtoMember(11), DefaultValue(80f)]
-		public float DialSpeedDegreesPerSecondMilkyway
-		{
-			get => _dialSpeedDegreesPerSecondMilkyway;
-			set
-			{
-				if (value < 20f) value = 20f;
-				if (value > 180f) value = 180f;
-				_dialSpeedDegreesPerSecondMilkyway = value;
-			}
-		}
-		[ProtoMember(12), DefaultValue(true)]
-		public bool AllowQuickDial { get; set; } = true;
-	}
+        [ProtoMember(1), DefaultValue(true)]
+        public bool DhdDestructable { get; set; } = true;
+        [ProtoMember(2), DefaultValue(4)]
+        public int DhdMiningTier { get; set; } = 4;
+        [ProtoMember(3), DefaultValue(true)]
+        public bool StargateDestructable { get; set; } = true;
+        [ProtoMember(4), DefaultValue(5)]
+        public int StargateMiningTier { get; set; } = 5;
+        [ProtoMember(5), DefaultValue(true)]
+        public bool VortexDestroys { get; set; } = true;
+        [ProtoMember(6), DefaultValue(true)]
+        public bool VortexKills { get; set; } = true;
+        [ProtoMember(7), DefaultValue(5f)]
+        public float MaxTimeoutSeconds { get; set; } = 5f;
+        [ProtoMember(8), DefaultValue(10)]
+        public int MinRangeChunksMilkyway
+        {
+            get => _minRangeChunksMilkyway;
+            set
+            {
+                if (value > _maxRangeChunksMilkyway) value = _maxRangeChunksMilkyway - 1;
+                if (value < 0) value = 0;
+                if (value > 262144) value = 262144;
+                _minRangeChunksMilkyway = value;
+            }
+        }
+        [ProtoMember(9), DefaultValue(262144)]
+        public int MaxRangeChunksMilkyway
+        {
+            get => _maxRangeChunksMilkyway;
+            set
+            {
+                if (value < _minRangeChunksMilkyway) value = _minRangeChunksMilkyway + 1;
+                if (value < 0) value = 0;
+                if (value > 262144) value = 262144;
+                _maxRangeChunksMilkyway = value;
+            }
+        }
+        [ProtoMember(10), DefaultValue(60f)]
+        public float MaxConnectionDurationSecondsMilkyway
+        {
+            get => _maxConnectionDurationSecondsMilkyway;
+            set
+            {
+                if (value < 10f) value = 10f;
+                if (value > 180f) value = 180f;
+                _maxConnectionDurationSecondsMilkyway = value;
+            }
+        }
+        [ProtoMember(11), DefaultValue(80f)]
+        public float DialSpeedDegreesPerSecondMilkyway
+        {
+            get => _dialSpeedDegreesPerSecondMilkyway;
+            set
+            {
+                if (value < 20f) value = 20f;
+                if (value > 180f) value = 180f;
+                _dialSpeedDegreesPerSecondMilkyway = value;
+            }
+        }
+        [ProtoMember(12), DefaultValue(true)]
+        public bool AllowQuickDial { get; set; } = true;
+
+        [ProtoMember(13)]
+        public int MinDistanceSurfaceGates
+        {
+            get => _minDistanceSurfaceGates;
+            set
+            {
+                if (value < 100) value = 100;
+                _minDistanceSurfaceGates = value;
+            }
+        }
+
+        [ProtoMember(14)]
+        public int MinDistanceUndergroundGates
+        {
+            get => _minDistanceUndergroundGates;
+            set
+            {
+                if (value < 100) value = 100;
+                _minDistanceUndergroundGates = value;
+            }
+        }
+    }
 }
--- src/Systems/StargateConfigSyncSystem.cs
+++ src/Systems/StargateConfigSyncSystem.cs
@@ -1,85 +1,129 @@
 ﻿using AstriaPorta.Config;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Server;
 using Vintagestory.Common;
+using Vintagestory.ServerMods;
 
 namespace AstriaPorta.src.Systems
 {
-	public class StargateConfigSystem : ModSystem
-	{
-		private const string configChannelName = "astriaportaconfigchannel";
-		private IServerNetworkChannel serverChannel;
-		private IClientNetworkChannel clientChannel;
+    public class StargateConfigSystem : ModSystem
+    {
+        private const string configChannelName = "astriaportaconfigchannel";
+        private IServerNetworkChannel serverChannel;
+        private IClientNetworkChannel clientChannel;
 
-		public override void StartPre(ICoreAPI api)
-		{
-			base.StartPre(api);
+        public override double ExecuteOrder()
+        {
+            // need to load after GenStructures ModSystem
+            return 0.35d;
+        }
 
-			setupNetworkChannels(api);
-			loadConfiguration(api);
+        public override void StartPre(ICoreAPI api)
+        {
+            setupNetworkChannels(api);
+            loadConfiguration(api);
 
-			if (api.Side == EnumAppSide.Server)
-			{
-				(api as ICoreServerAPI).Event.PlayerJoin += onPlayerJoined;
-			}
-		}
+            if (api.Side == EnumAppSide.Server)
+            {
+                (api as ICoreServerAPI).Event.PlayerJoin += onPlayerJoined;
+            }
+        }
 
-		private void setupNetworkChannels(ICoreAPI api)
-		{
-			if (api.Side == EnumAppSide.Server)
-			{
-				serverChannel = (api as ICoreServerAPI).Network.GetChannel(configChannelName);
-				if (serverChannel == null)
-				{
-					serverChannel = (api as ICoreServerAPI).Network.RegisterChannel(configChannelName);
-				}
-				serverChannel.RegisterMessageType<StargateConfig>();
-			} else
-			{
-				clientChannel = (api as ICoreClientAPI).Network.GetChannel(configChannelName);
-				if (clientChannel == null)
-				{
-					clientChannel = (api as ICoreClientAPI).Network.RegisterChannel(configChannelName);
-				}
-				clientChannel.RegisterMessageType<StargateConfig>();
-				clientChannel.SetMessageHandler<StargateConfig>(onReceivedServerConfig);
-			}
-		}
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            if (TerraGenConfig.DoDecorationPass)
+            {
+                api.Event.InitWorldGenerator(new Action(() => patchStructureParameters(api)), "standard");
+            }
+        }
 
-		private void onPlayerJoined(IServerPlayer player)
-		{
-			Mod.Logger.Notification($"Sending server-side config file to player {player.PlayerName}");
-			StargateConfig config = StargateConfig.Loaded;
-			serverChannel.SendPacket(StargateConfig.Loaded, new IServerPlayer[1] { player });
-		}
+        private void setupNetworkChannels(ICoreAPI api)
+        {
+            if (api.Side == EnumAppSide.Server)
+            {
+                serverChannel = (api as ICoreServerAPI).Network.GetChannel(configChannelName);
+                if (serverChannel == null)
+                {
+                    serverChannel = (api as ICoreServerAPI).Network.RegisterChannel(configChannelName);
+                }
+                serverChannel.RegisterMessageType<StargateConfig>();
+            }
+            else
+            {
+                clientChannel = (api as ICoreClientAPI).Network.GetChannel(configChannelName);
+                if (clientChannel == null)
+                {
+                    clientChannel = (api as ICoreClientAPI).Network.RegisterChannel(configChannelName);
+                }
+                clientChannel.RegisterMessageType<StargateConfig>();
+                clientChannel.SetMessageHandler<StargateConfig>(onReceivedServerConfig);
+            }
+        }
 
-		private void loadConfiguration(ICoreAPI api)
-		{
-			string filename = Mod.Info.ModID + ".json";
+        private void onPlayerJoined(IServerPlayer player)
+        {
+            Mod.Logger.Notification($"Sending server-side config file to player {player.PlayerName}");
+            StargateConfig config = StargateConfig.Loaded;
+            serverChannel.SendPacket(StargateConfig.Loaded, new IServerPlayer[1] { player });
+        }
 
-			StargateConfig config;
-			config = api.LoadModConfig<StargateConfig>(filename);
-			if (config == null)
-			{
-				api.StoreModConfig(StargateConfig.Loaded, filename);
-			}
-			else
-			{
-				StargateConfig.Loaded = config;
-			}
-		}
+        private void loadConfiguration(ICoreAPI api)
+        {
+            string filename = Mod.Info.ModID + ".json";
 
-		private void onReceivedServerConfig(StargateConfig serverConfig)
-		{
-			Mod.Logger.Notification("Received config file from server, will use that instead");
-			StargateConfig config = StargateConfig.Loaded;
-			StargateConfig.Loaded = serverConfig;
-		}
-	}
+            StargateConfig config;
+            config = api.LoadModConfig<StargateConfig>(filename);
+            if (config != null)
+            {
+                StargateConfig.Loaded = config;
+            }
+
+            api.StoreModConfig(StargateConfig.Loaded, filename);
+        }
+
+        private void onReceivedServerConfig(StargateConfig serverConfig)
+        {
+            Mod.Logger.Notification("Received config file from server, will use that instead");
+            StargateConfig.Loaded = serverConfig;
+        }
+
+        private void patchStructureParameters(ICoreServerAPI sapi)
+        {
+            StargateConfig config = StargateConfig.Loaded;
+            if (config == null) return;
+            GenStructures structuresSystem = sapi.ModLoader.GetModSystem<GenStructures>();
+            if (structuresSystem == null)
+            {
+                Mod.Logger.Debug("Couldn't find GenStructures modsystem, ignoring config settings related to structure generation");
+                return;
+            }
+
+            try
+            {
+
+                Type structuresSystemType = typeof(GenStructures);
+                WorldGenStructuresConfig structuresConfig = structuresSystemType.GetField("scfg", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(structuresSystem) as WorldGenStructuresConfig;
+
+                WorldGenStructure structure;
+                for (int i = 0; i < structuresConfig.Structures.Length; i++)
+                {
+                    structure = structuresConfig.Structures[i];
+                    if (structure.Group == "stargatesurface")
+                    {
+                        structure.MinGroupDistance = config.MinDistanceSurfaceGates;
+                    } else if (structure.Group == "stargateunderground")
+                    {
+                        structure.MinGroupDistance = config.MinDistanceUndergroundGates;
+                    }
+                }
+            } catch (Exception ex)
+            {
+                Mod.Logger.Debug(ex.Message);
+            }
+        }
+    }
 }
